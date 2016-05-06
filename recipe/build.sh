7f8f88a29f798ed4ae693e83982bdb855cbca585
Phil Elson <pelson.pub@gmail.com>
2016-05-06 16:57:51 +0100

    Enable compilation on OSX.

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,4 +15,12 @@ echo "udunits2_path = $PREFIX/lib/libudunits2.${EXT}" >> $SITECFG
 
 rm -rf lib/iris/tests/results lib/iris/tests/*.npz
 
-$PYTHON -sE setup.py --with-unpack build_ext "-I${PREFIX}/include" "-L${PREFIX}/lib" "-R${PREFIX}/lib" install --single-version-externally-managed --record record.txt
+$PYTHON -sE setup.py --with-unpack \
+    build_ext "-I${PREFIX}/include" "-L${PREFIX}/lib" "-R${PREFIX}/lib" \
+    install --single-version-externally-managed --record record.txt
+
+# Without this line, the OSX build fails reproducibly with
+# https://github.com/conda-forge/iris-feedstock/pull/2#issuecomment-217468453.
+# As a result, we have patched Iris in this recipe. This line should be able to be removed
+# once the patch is removed.
+$PYTHON -c "import iris.fileformats.netcdf; iris.fileformats.netcdf._pyke_kb_engine()"
